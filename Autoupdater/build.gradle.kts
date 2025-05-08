4d64ded1c74dbc3b3fa299beb286c7617bff4fcd
CSAbhiOnline <ovnaba2@gmail.com>
2025-05-08 11:37:11 +0530

    update gradle and android gradle plugin versions to 8.7.0

--- Autoupdater/build.gradle.kts
+++ Autoupdater/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.library") version "8.5.2"
+    id("com.android.library") version "8.7.0"
     id("org.jetbrains.kotlin.android") version "2.0.0"
     kotlin("plugin.serialization") version "2.0.20"
     `maven-publish`
